76f9d6209b5b9293493249d0d823ba5d3c215fe4
kevin-romero <elkenny@MacBook-Pro-de-El.local>
2022-11-23 09:02:01 -0800

    Se realizo el efecto popup a la seccion de skills

--- js/popUp.js
+++ js/popUp.js
@@ -1,23 +1,20 @@
 // * FADE
 
-altura = document.querySelector("#skills").offsetTop
-
 window.addEventListener('scroll', () => {
 
-        const element = document.querySelectorAll(`.popUp`);
+    const element = document.querySelectorAll(`.popUp`);
 
-        let i = 0;
+    let i = 0;
 
-        while(i < element.length){
+    while(i < element.length){
 
-            if(window.pageYOffset > (altura/1.4)){
-                    element[i].classList.add(`showPopUp`);
-            }else{
-                element[i].classList.remove(`showPopUp`);
-            }
-            i++;
+        if(window.pageYOffset > (document.querySelector("#skills").offsetTop/1.6)){
+                element[i].classList.add(`showPopUp`);
+        }else{
+            element[i].classList.remove(`showPopUp`);
         }
-
+        i++;
+    }
 });
 
 
