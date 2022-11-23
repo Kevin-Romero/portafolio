e0a07f974595db7d9c2919481343253e889ac3b7
kevin-romero <elkenny@MacBook-Pro-de-El.local>
2022-11-23 09:02:35 -0800

    Correcion de errores en el menu

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ const menu = document.querySelector("#menu");
 let altura = document.querySelector("#quiensoy").offsetTop;
 
 window.addEventListener("scroll", () => {
-    if(window.pageYOffset > (altura - 1)){
+    if(window.pageYOffset > (altura - 10)){
         menu.classList.add("scrollMenu");
     }else{
         menu.classList.remove("scrollMenu");
